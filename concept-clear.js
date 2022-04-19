dfa7dd3860e4dda5cfaf7d14f6cd32ffa9524d53
AKSarder <63703796+AKSarder@users.noreply.github.com>
2022-04-19 15:45:38 +0600

    file is done

---
+++ concept-clear.js
@@ -0,0 +1,10 @@
+// var number = 10;
+// console.log(typeof (number));
+// var name = "AK";
+// var AreYouThere = false;
+
+let number = 15;
+const number2 = 20;
+
+// number2 = 25;
+console.log(number2);
